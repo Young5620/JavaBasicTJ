01ae83ca62727daa2cb8333b25995232ea64d544
Young5620 <95738245+Young5620@users.noreply.github.com>
2021-12-15 16:59:58 +0900

    nn

---
+++ JavaInter/src/variableExam/TriExam.java
@@ -0,0 +1,20 @@
+package variableExam;
+
+public class TriExam {
+/*
+ 삼항연산자(조건)
+ (조건식) ? true : false
+ 첫번째 피연산자 조건식의 평가 결과에 따라 true면 두번째 항 false면 세번째 항의 값이 나온다.
+ 조건식의 괄호는 필수는 아니다.
+ if(조건식){
+ true;
+ }
+ else{
+ false;
+ }
+ */
+	public static void main(String[] args) {
+
+	}
+
+}
